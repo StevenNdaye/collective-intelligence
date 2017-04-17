11c56a2a282267fcf0447c8d7990c55ea00cfa70
Steven Ndaye <stevenndaye@gmail.com>
2017-04-17 17:06:23 +0200

    Added pearson similarity formula

--- collaborative-filtering/recommendations.py
+++ collaborative-filtering/recommendations.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
                          'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
                          'The Night Listener': 3.0},
@@ -30,3 +32,36 @@ def sim_distance(preferences, person1, person2):
     sum_of_squares = sum([pow(preferences[person1][item] - preferences[person2][item], 2)
                           for item in preferences[person1] if item in preferences[person2]])
     return 1 / (1 + sum_of_squares)
+
+
+def sim_pearson(preferences, person1, person2):
+    si = {}
+
+    for item in preferences[person1]:
+        if item in preferences[person2]:
+            si[item] = 1
+    n = len(si)
+
+    if n == 0:
+        return 0
+
+    sum1 = sum([preferences[person1][it] for it in si])
+    sum2 = sum([preferences[person2][it] for it in si])
+
+    sum1Sq = sum([pow(preferences[person1][it], 2) for it in si])
+    sum2Sq = sum([pow(preferences[person2][it], 2) for it in si])
+
+    pSum = sum([preferences[person1][it] * preferences[person2][it] for it in si])
+
+    num = pSum - (sum1 * sum2 / n)
+
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+
+    if den == 0:
+        return 0
+
+    r = num / den
+
+    return r
+
+
